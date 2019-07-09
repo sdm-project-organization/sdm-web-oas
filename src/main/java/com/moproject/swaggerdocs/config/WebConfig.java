dc44e87ccbe103e3c0a66bfa714a24581d1cf9f0
supermoon <supermoon@nexon.co.kr>
2019-07-09 16:08:03 +0900

    CORS all open

--- src/main/java/com/moproject/swaggerdocs/config/WebConfig.java
+++ src/main/java/com/moproject/swaggerdocs/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     // globally CORS (Cross-origin resource sharing)
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/static/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowCredentials(false)
                 .maxAge(3600);

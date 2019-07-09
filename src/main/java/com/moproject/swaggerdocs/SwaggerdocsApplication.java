cc5a353c186e0540dde0f75a03dcf9f6c0c2e118
supermoon <supermoon@nexon.co.kr>
2019-07-09 09:53:57 +0900

    first swagger docs

---
+++ src/main/java/com/moproject/swaggerdocs/SwaggerdocsApplication.java
@@ -0,0 +1,13 @@
+package com.moproject.swaggerdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerdocsApplication.class, args);
+    }
+
+}
